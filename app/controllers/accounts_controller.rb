36cf080f3fbeaddbcbb1f6bbbf7da1e3d6772049
Will Carron <wacarron27@gmail.com>
2015-07-14 12:19:37 -0700

    changed profile page.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -3,10 +3,10 @@ class AccountsController < ApplicationController
 		@account = current_user.accounts.build(:client_id = params[:client_id])
 		if @account.save
 			flash[:notice] = "Added client."
-			redirect_to root_url
+			redirect_to profile_view_path
 		else
 			flash[:notice] = "Unable to add client. Please try again."
-			redirect_to root_url
+			redirect_to new_user_registration_path
 		end
 	end
 end

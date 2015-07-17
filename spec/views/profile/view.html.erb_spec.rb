b20442a24b32cec081e2a625be23bc552cd93f43
Will Carron <wacarron27@gmail.com>
2015-07-17 12:11:51 -0700

    dashboard architecture changing to render partials based on role

--- spec/views/profile/view.html.erb_spec.rb
+++ spec/views/profile/view.html.erb_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe "profile/view.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+   scenario "with correct details", js: true do
+
+       create(:user, email: "someone@example.tld", password: "somepassword")
+
+       visit "/"
+
+       click_link "LOG IN"
+       expect(page).to have_css("h2", text: "LOGIN")
+       expect(current_path).to eq(new_user_session_path)
+
+       login "someone@example.tld", "somepassword"
+
+       expect(current_path).to eq "profile/view"
+      #  expect(page).to have_content ""
+      #  expect(page).to have_content "Hello, someone@example.tld"
+
+      #  click_button "Log out"
+       #
+      #  expect(current_path).to eq "/"
+      #  expect(page).to have_content "Signed out successfully"
+      #  expect(page).not_to have_content "someone@example.tld"
+
+     endend
